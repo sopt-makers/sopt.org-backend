c0a22cd3cc31890730d15d1f5ed1041bcb765c60
qkrwjdan <madogisa12@naver.com>
2022-09-27 12:15:22 +0900

    feat : typeorm으로 postgresql 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from 'src/configs/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeORMConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'root',
+  password: 'root',
+  database: 'test',
+  entities: [],
+  synchronize: true,
+};
