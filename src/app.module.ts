0790c40dc4e741a9104447d13455a7d9de169103
qkrwjdan <madogisa12@naver.com>
2022-09-27 12:58:53 +0900

    feat : db 정보 환경변수로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from 'src/configs/typeorm.config';
+import { typeORMFactory } from 'src/configs/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig)],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeORMFactory),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,17 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'root',
-  password: 'root',
-  database: 'test',
-  entities: [],
-  synchronize: true,
+export const typeORMFactory = {
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: +configService.get<number>('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_DATABASE'),
+    entities: [],
+    synchronize: true,
+  }),
+  inject: [ConfigService],
 };
