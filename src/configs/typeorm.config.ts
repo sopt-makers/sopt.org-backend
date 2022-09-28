ed61a946162a2bd484cb0c0b480869cfbe4dea6c
qkrwjdan <madogisa12@naver.com>
2022-09-29 03:35:14 +0900

    fix : db 연결 config 수정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -6,7 +6,7 @@ export const typeORMFactory = {
   useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
     type: 'postgres',
     host: configService.get('DB_HOST'),
-    port: +configService.get<number>('DB_PORT'),
+    port: configService.get<number>('DB_PORT'),
     username: configService.get('DB_USERNAME'),
     password: configService.get('DB_PASSWORD'),
     database: configService.get('DB_DATABASE'),
