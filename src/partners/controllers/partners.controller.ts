a25001f94bed2452e8d356425e6edf3239f89f33
qkrwjdan <madogisa12@naver.com>
2022-10-09 19:23:25 +0900

    feat : #4 GET /partners Mock API 추가

--- src/partners/controllers/partners.controller.ts
+++ src/partners/controllers/partners.controller.ts
@@ -1,13 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { GetPartnersDocs } from 'docs/partners/partners.swagger';
+import { PartnersResponseDto } from 'src/partners/dtos/partners-response.dto';
 
 @Controller('partners')
 @ApiTags('Partner')
 export class PartnersController {
   @Get('')
   @GetPartnersDocs()
-  async getPartners() {
-    return 'GET /partners';
+  async getPartners(): Promise<[PartnersResponseDto]> {
+    const mockPartner: PartnersResponseDto = {
+      id: 1,
+      name: 'Naver D2',
+      image:
+        'https://sopt-makers.s3.ap-northeast-2.amazonaws.com/mainpage/partners/logo/1660976077325_3EGL__X9xnpjgH3i49kKF.png',
+    };
+    return [mockPartner];
   }
 }
