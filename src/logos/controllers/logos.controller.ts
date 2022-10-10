823f44c65045dc5e0d41fe078d52cf2d896c7e6c
qkrwjdan <madogisa12@naver.com>
2022-10-11 00:57:02 +0900

    feat : #4 GET /logos Mock API 작성

--- src/logos/controllers/logos.controller.ts
+++ src/logos/controllers/logos.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { GetLogosDocs } from 'docs/logos/logos.swagger';
+import { LogosResponseDto } from 'src/logos/dtos/logos-response.dto';
 @ApiTags('Logos')
 @Controller('logos')
 export class LogosController {
   @Get('')
   @GetLogosDocs()
-  async getLogos() {
-    return 'GET /logos';
+  async getLogos(): Promise<[LogosResponseDto]> {
+    const mockLogo: LogosResponseDto = {
+      id: 1,
+      image:
+        'https://sopt-makers.s3.ap-northeast-2.amazonaws.com/mainpage/logo/1660922410081_AvI196XNRbXdWXpWQKlkg.png',
+    };
+    return [mockLogo];
   }
 }
