78e3a56117dc6effc9fa589168fa78918d0a44a9
qkrwjdan <madogisa12@naver.com>
2022-10-11 00:51:20 +0900

    docs : #4 GET /logos API 명세서 작성

--- src/logos/controllers/logos.controller.ts
+++ src/logos/controllers/logos.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-
+import { ApiTags } from '@nestjs/swagger';
+import { GetLogosDocs } from 'docs/logos/logos.swagger';
+@ApiTags('Logos')
 @Controller('logos')
 export class LogosController {
   @Get('')
+  @GetLogosDocs()
   async getLogos() {
     return 'GET /logos';
   }
--- src/logos/dtos/logos-response.dto.ts
+++ src/logos/dtos/logos-response.dto.ts
@@ -1,9 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LogosResponseDto {
-  @ApiProperty({ type: Number, required: true })
+  @ApiProperty({
+    type: Number,
+    required: true,
+    description: 'logo의 Id',
+  })
   id: number;
 
-  @ApiProperty({ type: String, required: true })
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: 'logo 이미지의 url 주소',
+  })
   image: string;
 }
