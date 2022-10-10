23924a043f8b668a87205af2e9891984cd1920b3
qkrwjdan <madogisa12@naver.com>
2022-10-11 01:27:13 +0900

    docs : #4 GET /cooperation-projects API 명세서 작성

--- src/cooperation-projects/controllers/cooperation-projects.controller.ts
+++ src/cooperation-projects/controllers/cooperation-projects.controller.ts
@@ -1,8 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { GetCooperationProjectsDocs } from 'docs/cooperation-projects/cooperation-projects.swagger';
 
+@ApiTags('CooperationProject')
 @Controller('cooperation-projects')
 export class CooperationProjectsController {
   @Get()
+  @GetCooperationProjectsDocs()
   async getCooperationProjects() {
     return 'GET /cooperation-projects';
   }
