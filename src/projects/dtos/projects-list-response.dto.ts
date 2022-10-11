cf9a51581cdbbb9b387d7439011596b99d9578dc
qkrwjdan <madogisa12@naver.com>
2022-10-11 14:22:37 +0900

    feat : #4 ProjectsListResponseDto 생성

---
+++ src/projects/dtos/projects-list-response.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ProjectsResponseDto } from 'src/projects/dtos/projects-response.dto';
+
+export class ProjectsListResponseDto {
+  @ApiProperty({
+    type: [ProjectsResponseDto],
+    required: true,
+  })
+  projects: [ProjectsResponseDto];
+
+  @ApiProperty({
+    type: Boolean,
+    required: true,
+    description:
+      '추가 정보 유무. 더 가져올 데이터가 있으면 true, 더 가져올 데이터가 없는 경우 false',
+  })
+  hasMore: boolean;
+}
