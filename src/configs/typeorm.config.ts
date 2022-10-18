55e0881150b2246797c3fd22ca240e810e451f9f
qkrwjdan <madogisa12@naver.com>
2022-10-18 19:00:24 +0900

    hotfix : typeorm config 중 synchronize false로 변경, entities 설정 변경

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -10,8 +10,8 @@ export const typeORMFactory = {
     username: configService.get('DB_USERNAME'),
     password: configService.get('DB_PASSWORD'),
     database: configService.get('DB_DATABASE'),
-    entities: [],
-    synchronize: true,
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: false,
   }),
   inject: [ConfigService],
 };
