70016ad17c15f16080528394d796ad9909f8db77
qkrwjdan <madogisa12@naver.com>
2022-10-18 21:08:31 +0900

    feat : #11 GET /partners API 개발

--- src/partners/partners.module.ts
+++ src/partners/partners.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Partner } from 'src/partners/entities/partners.entity';
+import { PartnersService } from 'src/partners/services/partners.service';
 import { PartnersController } from './controllers/partners.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Partner])],
+  providers: [PartnersService],
   controllers: [PartnersController],
 })
 export class PartnersModule {}
