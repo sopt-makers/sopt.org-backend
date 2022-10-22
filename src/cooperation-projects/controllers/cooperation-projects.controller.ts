87a3927811c56bf3135341fded49c5dde2135ace
qkrwjdan <madogisa12@naver.com>
2022-10-22 22:59:28 +0900

    feat : #13 GET /cooperation-projects API 개발

--- src/cooperation-projects/controllers/cooperation-projects.controller.ts
+++ src/cooperation-projects/controllers/cooperation-projects.controller.ts
@@ -2,24 +2,40 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { GetCooperationProjectsDocs } from 'docs/cooperation-projects/cooperation-projects.swagger';
 import { CooperationProjectsResponseDto } from 'src/cooperation-projects/dtos/cooperation-projects-response.dto';
+import { CooperationProject } from 'src/cooperation-projects/entities/cooperation-projects.entity';
+import { CooperationProjectsService } from 'src/cooperation-projects/services/cooperation-projects.service';
+
+function getCooperationProjectsResponseDto(
+  cooperationProjects: CooperationProject[],
+): CooperationProjectsResponseDto[] {
+  return cooperationProjects.map((cooperationProject: CooperationProject) => {
+    return {
+      id: cooperationProject.id,
+      year: cooperationProject.year ? cooperationProject.year : 0,
+      title: cooperationProject.title ? cooperationProject.title : '',
+      content: cooperationProject.content ? cooperationProject.content : '',
+      subContent: cooperationProject.subContent
+        ? cooperationProject.subContent
+        : '',
+      posterImage: cooperationProject.posterImage
+        ? cooperationProject.posterImage
+        : '',
+    };
+  });
+}
 
 @ApiTags('CooperationProject')
 @Controller('cooperation-projects')
 export class CooperationProjectsController {
+  constructor(
+    private readonly cooperationProjectsService: CooperationProjectsService,
+  ) {}
   @Get()
   @GetCooperationProjectsDocs()
   async getCooperationProjects(): Promise<
     Array<CooperationProjectsResponseDto>
   > {
-    const mockCooperationProject: CooperationProjectsResponseDto = {
-      id: 11,
-      year: 2022,
-      title: 'KB D.N.A 프로젝트',
-      content: `KB금융그룹과 SOPT, 디지털 전문가가 협업하여 KB 디지털 프로젝트의 개선과제를 발굴하고, 이를 해결하기 위한 구체적인 아이디어를 도출해 프로토타입을 제작하는 프로젝트`,
-      subContent: '2018년부터 총 5회 진행',
-      posterImage:
-        'https://sopt-makers.s3.ap-northeast-2.amazonaws.com/mainpage/partners/poster/1661005789051_chHEt5ezSXlTRKfHmRnRv.png',
-    };
-    return [mockCooperationProject];
+    const cooperationProjects = await this.cooperationProjectsService.findAll();
+    return getCooperationProjectsResponseDto(cooperationProjects);
   }
 }
---
+++ src/cooperation-projects/services/cooperation-projects.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CooperationProject } from 'src/cooperation-projects/entities/cooperation-projects.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class CooperationProjectsService {
+  constructor(
+    @InjectRepository(CooperationProject)
+    private cooperationProjectsRepository: Repository<CooperationProject>,
+  ) {}
+
+  findAll(): Promise<CooperationProject[]> {
+    return this.cooperationProjectsRepository.find();
+  }
+}
