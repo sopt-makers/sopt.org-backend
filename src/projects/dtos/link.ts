792694bcf6bbb2eccf83056edad82ac8ba3d52fd
qkrwjdan <madogisa12@naver.com>
2022-11-04 02:34:16 +0900

    feat : #14 link의 type을 enum으로 수정

--- src/projects/dtos/link.ts
+++ src/projects/dtos/link.ts
@@ -1,11 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
 
+export enum LinkType {
+  WEBSITE = '웹사이트',
+  GOOGLE_PLAYSTORE = '구글 플레이스토어',
+  APP_STORE = '앱 스토어',
+  GITHUB = 'Github',
+  PRESENTATION_VIDEO = '발표영상',
+  INSTAGRAM = 'instagram',
+  ETC = '기타 관련자료',
+}
+
 export class Link {
   @ApiProperty({
     type: String,
     required: true,
     description:
       '웹사이트, 구글 플레이스토어, 앱스토어, Github, 발표영상 등 프로젝트에 관련된 링크의 종류',
+    enum: LinkType,
   })
   title: string;
 
