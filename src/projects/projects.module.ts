b5ccee90e76e7770046bf0e8c4686d316f03c234
qkrwjdan <madogisa12@naver.com>
2022-11-18 23:49:44 +0900

    [feat] playground 팀 API 호출해 projects 정보 가져와 GET /projects API 개발

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,8 +1,10 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { projectsService } from 'src/projects/services/projects.service';
 import { ProjectsController } from './controllers/projects.controller';
 
 @Module({
+  imports: [HttpModule],
   providers: [projectsService],
   controllers: [ProjectsController],
 })
