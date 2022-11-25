6198e275fdeede7f2b5b432bc612c4a244e467f2
qkrwjdan <madogisa12@naver.com>
2022-11-26 00:23:29 +0900

    feat : #15 GET /semesters API 개발

--- src/semesters/semesters.module.ts
+++ src/semesters/semesters.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SemestersController } from './controllers/semesters.controller';
 import { SemestersDetailController } from './controllers/semesters-detail.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SemestersService } from 'src/semesters/services/semesters.service';
+import { Semester } from 'src/semesters/entities/semesters.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Semester])],
+  providers: [SemestersService],
   controllers: [SemestersController, SemestersDetailController],
 })
 export class SemestersModule {}
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,3 +8,6 @@ export const env = {
   development: 'development',
   local: 'local',
 };
+
+export const DEFAULT_SEMESTER_LOGO =
+  'https://sopt-makers.s3.ap-northeast-2.amazonaws.com/mainpage/semester/logo/default.png';
