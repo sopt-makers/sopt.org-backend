1da68a7f4f39ce2dd8d5a7af783518fe5f85e690
qkrwjdan <madogisa12@naver.com>
2023-02-03 15:26:54 +0900

    hotfix : 디테일 페이지에서 멤버가 보이지 않는 오류 수정

--- src/projects/services/projects.service.ts
+++ src/projects/services/projects.service.ts
@@ -62,15 +62,18 @@ export class projectsService {
       return link;
     });
 
-    const members: Array<Member> = [];
-    // const members: Array<Member> = response.members.map((data) => {
-    //   const member: Member = {
-    //     name: data.memberName,
-    //     role: data.memberRole,
-    //     description: data.memberDescription,
-    //   };
-    //   return member;
-    // });
+    let members: Array<Member> = [];
+
+    if (response.members) {
+      members = response.members.map((data) => {
+        const member: Member = {
+          name: data.memberName,
+          role: data.memberRole,
+          description: data.memberDescription,
+        };
+        return member;
+      });
+    }
 
     return {
       id: response.id,
@@ -110,6 +113,19 @@ export class projectsService {
         })
         .pipe(map((res) => res.data)),
     );
+
+    // 중복제거 로직 : 추후 제거 예정
+    const uniqueResponse: PlaygroundProjectResponseDto[] = dropDuplication(
+      response,
+      'name',
+    );
+    uniqueResponse.forEach((response) => {
+      response.links = dropDuplication(response.links, 'linkId');
+    });
+
+    if (!uniqueResponse) {
+      return [];
+    }
 
     for (const data of response) {
       res.push(this.getProjectResponseDto(data));
