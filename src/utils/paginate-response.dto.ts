e9a10ed5670563d046714b65544d32d38f4b182f
dudgns3tp <c_yh0327@naver.com>
2023-03-16 22:55:25 +0900

    feat: Add paginate response dto Api Property

--- src/utils/paginate-response.dto.ts
+++ src/utils/paginate-response.dto.ts
@@ -1,10 +1,53 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class PaginateResponseDto<T> {
+  @ApiProperty({
+    type: Array,
+    nullable: false,
+    description: 'item',
+  })
   data: T[];
+
+  @ApiProperty({
+    type: Boolean,
+    nullable: false,
+    description: '다음 페이지가 있는지 여부를 나타냄.',
+  })
   hasNextPage: boolean;
+
+  @ApiProperty({
+    type: Boolean,
+    nullable: false,
+    description: '이전 페이지가 있는지 여부를 나타냄.',
+  })
   hasPrevPage: boolean;
+
+  @ApiProperty({
+    type: Number,
+    nullable: false,
+    description: '총 data 들의 갯수',
+  })
   totalCount: number;
+
+  @ApiProperty({
+    type: Number,
+    nullable: false,
+    description: '총 페이지 카운트',
+  })
   totalPage: number;
+
+  @ApiProperty({
+    type: Number,
+    nullable: false,
+    description: '현재 페이지',
+  })
   currentPage: number;
+
+  @ApiProperty({
+    type: Number,
+    nullable: false,
+    description: 'item을 몇개까지 가져올지에 대한 카운트',
+  })
   limit: number;
 
   constructor(
