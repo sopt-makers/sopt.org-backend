01cf8992c154ad93f2cf85516815a675f9567408
dudgns3tp <c_yh0327@naver.com>
2023-03-17 10:10:41 +0900

    fix: DTO type Transform
    PageNo, Limit Query가 string으로 들어와서 Number로 Transform 하였고, Query값들을 optional로 받도록 수정

--- src/utils/paginate-request.dto.ts
+++ src/utils/paginate-request.dto.ts
@@ -1,21 +1,27 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+
 export abstract class PageRequest {
   @ApiProperty({
     type: Number,
     default: 1,
     required: false,
   })
+  @IsOptional()
   @IsNumber()
-  pageNo: number | 1;
+  @Transform(({ value }) => parseInt(value), { toClassOnly: true })
+  pageNo: number = 1;
 
   @ApiProperty({
     type: Number,
     default: 10,
     required: false,
   })
+  @IsOptional()
   @IsNumber()
-  limit: number | 10;
+  @Transform(({ value }) => parseInt(value), { toClassOnly: true })
+  limit: number = 10;
 
   getOffset(): number {
     return (this.pageNo - 1) * this.limit;
