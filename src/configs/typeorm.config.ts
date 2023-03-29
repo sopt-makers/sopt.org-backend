9a389b0044c33b197b6ac2888d2761132aedc78f
dudgns3tp <c_yh0327@naver.com>
2023-03-30 00:22:40 +0900

    refactor: add generic configService

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,9 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EnvConfig } from './env.config';
 
 export const typeORMFactory = {
   imports: [ConfigModule],
-  useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+  useFactory: (
+    configService: ConfigService<EnvConfig>,
+  ): TypeOrmModuleOptions => ({
     type: 'postgres',
     host: configService.get('DB_HOST'),
     port: configService.get<number>('DB_PORT'),
