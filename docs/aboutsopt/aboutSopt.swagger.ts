c8cf09ce6709fe9a6a69f03537168eb6f6d72ace
dudgns3tp <c_yh0327@naver.com>
2023-04-27 01:02:20 +0900

    chore: remove extractSchema

--- docs/aboutsopt/aboutSopt.swagger.ts
+++ docs/aboutsopt/aboutSopt.swagger.ts
@@ -1,11 +1,6 @@
 import { applyDecorators } from '@nestjs/common';
 import { AboutSoptResponseDto } from '../../src/aboutsopt/dtos/aboutsopt-response.dto';
-import {
-  ApiExtraModels,
-  ApiOkResponse,
-  ApiOperation,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 export function GetAdminAboutSoptDocs() {
   return applyDecorators(
@@ -13,7 +8,6 @@ export function GetAdminAboutSoptDocs() {
     ApiOperation({
       summary: 'Admin용 AboutSopt 조회, 해당 기수의 AboutSOPT가  없으면 생성',
     }),
-    ApiExtraModels(AboutSoptResponseDto),
     ApiOkResponse({ type: AboutSoptResponseDto }),
   );
 }
