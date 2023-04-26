4b1cad125a6d23af1337be2458f443fee5172d20
dudgns3tp <c_yh0327@naver.com>
2023-04-26 22:44:31 +0900

    feat: update AboutSopt

--- src/aboutsopt/controllers/aboutsopt.controller.ts
+++ src/aboutsopt/controllers/aboutsopt.controller.ts
@@ -1,34 +1,34 @@
-import { AboutSoptPostDto } from '../dtos/aboutsopt-post.dto';
 import {
   Body,
   Controller,
   Get,
   Param,
-  Post,
+  Put,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 
 import { AboutSoptService } from '../services/aboutsopt.service';
 import { AboutSoptResponseDto } from '../dtos/aboutsopt-response.dto';
+import { AboutSoptUpdateDto } from '../dtos/aboutsopt-update.dto';
 
 @UsePipes(new ValidationPipe({ transform: true }))
 @Controller('aboutsopt')
 export class AboutSoptController {
-  constructor(private readonly aboutsoptService: AboutSoptService) {}
+  constructor(private readonly aboutSoptService: AboutSoptService) {}
 
-  @Get('/semester/:id/admin')
+  @Get('admin/semester/:id')
   async getOrInit(
     @Param('id') id: number,
   ): Promise<AboutSoptResponseDto | null> {
-    return this.aboutsoptService.getOrInit(id);
+    return this.aboutSoptService.getOrInit(id);
   }
 
-  @Post()
-  async postAboutSopt(
-    @Body() aboutSoptPostDto: AboutSoptPostDto,
-  ): Promise<AboutSoptResponseDto | null> {
-    console.log(aboutSoptPostDto);
-    return this.aboutsoptService.postAboutSopt(aboutSoptPostDto);
+  @Put('admin/semester/:id')
+  async updateAboutSopt(
+    @Body() aboutSoptPostDto: AboutSoptUpdateDto,
+    @Param('id') id: number,
+  ): Promise<AboutSoptResponseDto> {
+    return this.aboutSoptService.updateAboutSopt(id, aboutSoptPostDto);
   }
 }
--- src/aboutsopt/services/aboutsopt.service.ts
+++ src/aboutsopt/services/aboutsopt.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { AboutSoptPostDto } from '../dtos/aboutsopt-post.dto';
 import { AboutSopt } from '../entities/aboutsopt.entity';
 import { CoreValue } from '../entities/coreValue.entity';
+import {
+  AboutSoptUpdateDto,
+  CoreValueUpdateDto,
+} from '../dtos/aboutsopt-update.dto';
 
 @Injectable()
 export class AboutSoptService {
@@ -50,12 +53,56 @@ export class AboutSoptService {
     return this.aboutSoptRepository.findOne({ where: { id: id } });
   }
 
-  async postAboutSopt(
-    aboutSoptPostDto: AboutSoptPostDto,
-  ): Promise<AboutSopt | null> {
-    const aboutsopt = await this.aboutSoptRepository.save(aboutSoptPostDto);
+  async updateAboutSopt(
+    id: number,
+    aboutSoptPostDto: AboutSoptUpdateDto,
+  ): Promise<AboutSopt> {
+    const aboutSopt = await this.aboutSoptRepository.findOne({
+      where: { id: id },
+      order: {
+        coreValues: {
+          id: 'asc',
+        },
+      },
+    });
+    if (!aboutSopt) {
+      throw new NotFoundException('Not found about sopt with id: ' + id);
+    }
 
-    const id = aboutSoptPostDto.id;
-    return await this.getOrInit(id);
+    this.validateAboutSoptPostDto(aboutSopt, aboutSoptPostDto);
+
+    aboutSopt.bannerImage = aboutSoptPostDto.bannerImage;
+    aboutSopt.coreDescription = aboutSoptPostDto.coreDescription;
+    aboutSopt.planCurriculum = aboutSoptPostDto.planCurriculum;
+    aboutSopt.androidCurriculum = aboutSoptPostDto.androidCurriculum;
+    aboutSopt.designCurriculum = aboutSoptPostDto.designCurriculum;
+    aboutSopt.iosCurriculum = aboutSoptPostDto.iosCurriculum;
+    aboutSopt.serverCurriculum = aboutSoptPostDto.serverCurriculum;
+    aboutSopt.webCurriculum = aboutSoptPostDto.webCurriculum;
+
+    aboutSopt.coreValues.map((coreValue) => {
+      const coreValueDto = aboutSoptPostDto.coreValues.find(
+        (coreValueDto) => coreValueDto.id === coreValue.id,
+      ) as CoreValueUpdateDto;
+      coreValue.title = coreValueDto.title;
+      coreValue.subTitle = coreValueDto.subTitle;
+      coreValue.imageUrl = coreValueDto.imageUrl;
+      return this.coreValueRepository.save(coreValue);
+    });
+    return this.aboutSoptRepository.save(aboutSopt);
+  }
+
+  private validateAboutSoptPostDto(
+    aboutSopt: AboutSopt,
+    aboutSoptPostDto: AboutSoptUpdateDto,
+  ): void {
+    const coreValueIds = aboutSopt.coreValues.map((coreValue) => coreValue.id);
+    aboutSoptPostDto.coreValues.forEach((coreValueDto) => {
+      if (!coreValueIds.includes(coreValueDto.id)) {
+        throw new NotFoundException(
+          'Not found core value with id: ' + coreValueDto.id,
+        );
+      }
+    });
   }
 }
