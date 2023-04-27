0742567d6b0bc02e793b80d8ccafd4d0b253ab0f
최영훈 <40652160+dudgns3tp@users.noreply.github.com>
2023-04-27 13:32:42 +0900

    chore: typeorm sync false

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -15,7 +15,7 @@ export const typeORMFactory = {
     database: configService.get('DB_DATABASE'),
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     logging: configService.get('NODE_ENV') === 'development',
-    synchronize: true,
+    synchronize: false,
   }),
   inject: [ConfigService],
 };
