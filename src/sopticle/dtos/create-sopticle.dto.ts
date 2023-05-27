2e9366da013737848d73156bed4a9ecc3b03e7d9
dudgns3tp <c_yh0327@naver.com>
2023-05-27 16:34:50 +0900

    feat: Sopticle Url Validation

--- src/sopticle/dtos/create-sopticle.dto.ts
+++ src/sopticle/dtos/create-sopticle.dto.ts
@@ -1,13 +1,64 @@
-import { IsString, IsUrl } from 'class-validator';
+import {
+  IsString,
+  IsUrl,
+  registerDecorator,
+  ValidationOptions,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+
+export const validateVelogURL = (url: string): boolean => {
+  const regex = /^https:\/\/velog\.io\/@[^/]+(\/[^/]+)?\/?$/;
+  return regex.test(url);
+};
+
+export const validateTistoryURL = (url: string): boolean => {
+  const regex = /^https:\/\/\w+\.tistory\.com\/[^/]+\/?$/;
+  return regex.test(url);
+};
+
+export const validateBrunchURL = (url: string): boolean => {
+  const regex = /^https:\/\/brunch\.co\.kr\/[^/]+\/[^/]+\/?$/;
+  return regex.test(url);
+};
+
+export function IsValidateSopticlePlatformUrl(
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'IsValidateSopticlePlatformUrl',
+      target: object.constructor,
+      async: false,
+      propertyName: propertyName,
+      constraints: [],
+      options: validationOptions,
+      validator: {
+        validate(value: string): boolean {
+          if (value.includes('velog.io')) {
+            return validateVelogURL(value);
+          }
+          if (value.includes('tistory.com')) {
+            return validateTistoryURL(value);
+          }
+          if (value.includes('brunch.co.kr')) {
+            return validateBrunchURL(value);
+          }
+          return false;
+        },
+      },
+    });
+  };
+}
 
 export class CreateSopticleDto {
   @ApiProperty({
     type: String,
     required: true,
-    description: '솝티클 주소, Velog, Brunch, Tistory 등',
+    description:
+      '솝티클 주소 입니다. Velog, Brunch, Tistory  플랫폼만 가능합니다.',
   })
   @IsUrl()
   @IsString()
+  @IsValidateSopticlePlatformUrl({ message: 'Invalid url' })
   readonly sopticleUrl: string;
 }
