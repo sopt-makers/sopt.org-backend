b48d1caf4af458a42ea9809226ea1e7e91788b47
dudgns3tp <c_yh0327@naver.com>
2023-05-27 21:06:05 +0900

    feat: tisotry strategy

--- src/scraper/strategy/tisotry-scraping.strategy.ts
+++ src/scraper/strategy/tisotry-scraping.strategy.ts
@@ -1,3 +1,6 @@
+import { Page } from 'puppeteer';
+import { InternalServerErrorException } from '@nestjs/common';
+
 import { ScrapingStrategy } from './scrap.strategy';
 import { PuppeteerService } from '../puppeteer.service';
 import { CreateScraperResponseDto } from '../dto/create-scraper-response.dto';
@@ -8,12 +11,56 @@ export class TisotryScrapingStrategy extends ScrapingStrategy {
     super(url, puppeteerService);
   }
   async onLoad(): Promise<CreateScraperResponseDto> {
+    const browser = await this.puppeteerService.getBrowser();
+    const page = await browser.newPage();
+    await page.goto(this.url);
+    const title = await this.getTitle(page);
+    const description = await this.getDescription(page);
+    const image = await this.getImage(page);
     return {
-      thumbnailUrl: 'zz',
-      title: 'zz',
-      description: 'ㅋㅋ',
+      title,
+      description,
+      thumbnailUrl: image,
       sopticleUrl: this.url,
       platform: SopticlePlatformEnum.TISTORY,
     };
+  }
+
+  private async getImage(page: Page): Promise<string> {
+    const image = await page.$eval('meta[property="og:image"]', (element) =>
+      element.getAttribute('content'),
+    );
+
+    if (image === null) {
+      console.error('Tistory get Image Null, url: ', this.url);
+      throw new InternalServerErrorException('Tistory Image Null');
+    }
+
+    return image;
+  }
+
+  private async getDescription(page: Page): Promise<string> {
+    const description = await page.$eval(
+      'meta[property="og:description"]',
+      (element) => element.getAttribute('content'),
+    );
+
+    if (description === null) {
+      console.error('Tistory get Description Null, url: ', this.url);
+      throw new InternalServerErrorException('Tistory Description Null');
+    }
+    return description;
+  }
+
+  private async getTitle(page: Page): Promise<string> {
+    const title = await page.$eval('meta[property="og:title"]', (element) =>
+      element.getAttribute('content'),
+    );
+
+    if (title === null) {
+      console.error('Tistory get Title Null, url: ', this.url);
+      throw new InternalServerErrorException('Tistory Title Null');
+    }
+    return title;
   }
 }
