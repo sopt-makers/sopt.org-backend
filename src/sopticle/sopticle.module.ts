ca1c746dd8d6a4289cc0fe8f73c3ac6f49bc4961
dudgns3tp <c_yh0327@naver.com>
2023-06-03 01:36:38 +0900

    refactor: remove SopticleFactoryService

--- src/sopticle/sopticle.module.ts
+++ src/sopticle/sopticle.module.ts
@@ -6,7 +6,6 @@ import { SopticleController } from './controllers/sopticle.controller';
 import { SopticleService } from './services/sopticle.service';
 import { PlaygroudModule } from '../internal/playground/playgroud.module';
 import { ScraperModule } from '../scraper/scraper.module';
-import { SopticleFactoryService } from './services/sopticle-factory.service';
 
 @Module({
   imports: [
@@ -15,6 +14,6 @@ import { SopticleFactoryService } from './services/sopticle-factory.service';
     ScraperModule,
   ],
   controllers: [SopticleController],
-  providers: [SopticleService, SopticleFactoryService],
+  providers: [SopticleService],
 })
 export class SopticleModule {}
