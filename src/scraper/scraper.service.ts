159dac05c965af2fad506ed4912017ccf9a00586
찬기 <limchangi@naver.com>
2023-10-02 20:33:33 +0900

    debug: puppeteer docker command recommendation

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -100,7 +100,13 @@ export class ScraperService {
   ): Promise<CreateScraperResponseDto> {
     const browser = await puppeteer.launch({
       headless: 'new',
-      args: ['--no-sandbox'],
+      executablePath: '/usr/bin/google-chrome',
+      args: [
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-setuid-sandbox',
+        '--no-sandbox',
+      ],
     });
     const page = await browser.newPage();
 
