8f552713fd0a2ab68c412c0a08854845194bc4c8
찬기 <limchangi@naver.com>
2023-10-02 19:52:08 +0900

    debug: puppeteer docker line

--- .puppeteerrc.cjs
+++
@@ -1,8 +0,0 @@
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-    // Changes the cache location for Puppeteer.
-    cacheDirectory: "/home/ubuntu/.cache/puppeteer",
-    skipDownload: true
-};
