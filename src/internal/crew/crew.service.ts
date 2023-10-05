559369661885662c4c10d609994d25c8b3154d88
찬기 <limchangi@naver.com>
2023-10-05 15:20:01 +0900

    feat: Study API 32기 조회하도록 수정

--- src/internal/crew/crew.service.ts
+++ src/internal/crew/crew.service.ts
@@ -9,16 +9,20 @@ export class CrewService {
 
   async getStudies(): Promise<StudyResponseDto[]> {
     const MAX_TAKE_COUNT = 50;
-    const response = await this.crewRepository.findAll({
-      page: 1,
-      limit: MAX_TAKE_COUNT,
-    });
+    const PREVIOUS_GEN = 32; // TODO. 임시로 집어넣은 부분이라 추후 클라와 상의 후, 기수를 입력받는 형식으로 API를 수정해야함!
+    const response = await this.crewRepository.findAll(
+      {
+        page: 1,
+        limit: MAX_TAKE_COUNT,
+      },
+      PREVIOUS_GEN,
+    );
 
     return response.data.meetings.map(
       (meeting: CrewMeetingDto): StudyResponseDto => {
         return {
           id: meeting.id,
-          generation: meeting.targetActiveGeneration,
+          generation: meeting.createdGeneration,
           parts: meeting.joinableParts,
           title: meeting.title,
           imageUrl: meeting.imageURL.length ? meeting.imageURL[0].url : null,
