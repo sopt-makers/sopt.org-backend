bf37061898867272f42706009efa005ab71d7b3a
찬기 <limchangi@naver.com>
2023-10-25 16:44:21 +0900

    feat: Review and Sopticle Generation Query 작업

--- src/reviews/dtos/reviews-request.dto.ts
+++ src/reviews/dtos/reviews-request.dto.ts
@@ -1,6 +1,8 @@
 import { PageRequest } from '../../utils/paginate-request.dto';
 import { ApiProperty } from '@nestjs/swagger';
 import { Part } from '../../common/type';
+import { IsNumber, IsOptional } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class ReviewsRequestDto extends PageRequest {
   @ApiProperty({
@@ -10,4 +12,15 @@ export class ReviewsRequestDto extends PageRequest {
     description: 'Part, 전체를 불러올땐 아무값도 안넣으면 됩니다.',
   })
   part?: Part;
+
+  @ApiProperty({
+    required: false,
+    description:
+      '활동기수로 필터링 합니다, 값을 넣지 않을 경우 전체 조회합니다.',
+    type: Number,
+  })
+  @Transform(({ value }) => parseInt(value))
+  @IsNumber()
+  @IsOptional()
+  readonly generation: number | null;
 }
